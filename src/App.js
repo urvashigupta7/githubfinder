11bd04332a4b7742129a0be23f711c0bbd734bf6
root <urvashi072000@gmail.com>
2019-10-01 15:53:24 +0000

    added search functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/navbar';
 import User from './components/user/users';
+import Search from './components/user/search'; 
 
 
 class App extends React.Component {
@@ -9,21 +10,27 @@ class App extends React.Component {
 		users:[],
 		loading:false
 	}
-	async componentDidMount(){
-		this.setState({loading:true});
-		const res= await fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${ process.env.REACT_APP_CLIENT_SECRET}`);
-		
+searchuser=async(text)=>{
+	this.setState({loading:true});
+	const res= await fetch(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}
+&client_secret=${ process.env.REACT_APP_CLIENT_SECRET}`);
 		res.json().then((data)=>{
-			this.setState({users:data});
+			this.setState({users:data.items})
 		})
-		this.setState({loading:false});
-	
-	}
+	this.setState({loading:false});
+}
+clearuser=()=>{
+	this.setState({
+	    users:[]})
+}	
 	render(){
+	
 		 return (
     <div className="App">
 		  <Navbar title='GitHub Finder'/>
+				
 				 <div className="container">
+				<Search searchuser={this.searchuser}  clearUser={this.clearuser} showclear={this.state.users.length>0?true:false}/>
 					 <User loading={this.state.loading} users={this.state.users}/>
 				 </div>
 		
---
+++ src/components/user/search.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react';
+import PropTypes from 'prop-types'
+class Search extends Component{
+	state={
+			text:''
+		}
+		onChange=(e)=>
+		{this.setState({[e.target.name]:e.target.value})
+		}
+		onSubmit=(e)=>{
+			e.preventDefault();
+			this.props.searchuser(this.state.text);
+			this.setState({text:' '});
+		}
+		static propTypes={
+			searchuser:PropTypes.func.isRequired,
+			clearUser:PropTypes.func.isRequired,
+			showclear:PropTypes.bool.isRequired
+		}
+	render(){
+		return( 
+		<div>
+				<form onSubmit={this.onSubmit} className='form'>
+					<input type='text' name='text' placeholder='Search Users...' value={this.state.text}
+						onChange={this.onChange}/>
+					<input
+						type='submit'
+						value='search'
+						className='btn btn-dark btn-block' /> 
+				</form>
+				{this.props.showclear&&(
+				<button className='btn btn-light btn-block' onClick={this.props.clearUser}>Clear</button>
+					)}
+				
+			</div>
+		)
+	}
+}
+export default Search;
